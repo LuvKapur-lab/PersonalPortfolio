91d0106743cc720b91427c5eb9c97ec0791c2898
luv Kapur <luvkapur4@gmail.com>
2025-03-23 22:48:59 +0530

    made some changes

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
-let btn = document.querySelector(".btn")
-let lightMode = document.querySelector(".to-light-mode");
-let darkMode = document.querySelector(".to-dark-mode");
-let body = document.querySelector("body");
-let home = document.querySelector(".home-info");
-let h1 = document.querySelector(".f-h1");
-let a = document.querySelectorAll("a");
-let p = document.querySelectorAll("p");
-let icon = document.querySelector("img")
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.querySelector(".btn");
+  let lightMode = document.querySelector(".to-light-mode");
+  let darkMode = document.querySelector(".to-dark-mode");
+  let body = document.querySelector("body");
+  let home = document.querySelector(".home-info");
+  let h1 = document.querySelectorAll("h1");
+  let a = document.querySelectorAll("a");
+  let p = document.querySelectorAll("p");
+  let icon = document.querySelector("img");
 
-let isLight = true
-btn.addEventListener("click", function () {
-  if (isLight) {
-    dark();
-  }else{
-    light()
+  let isLight = true;
+  btn.addEventListener("click", function () {
+    if (isLight) {
+      dark();
+    } else {
+      light();
+    }
+    isLight = !isLight;
+  });
+
+  function light() {
+    body.style.backgroundColor = "#FAF9F6";
+
+    p.forEach((e) => {
+      e.style.color = "rgb(65, 65, 65)";
+    });
+
+    h1.forEach((elem) => {
+      elem.style.color = "black";
+    });
+
+    a.forEach((e) => {
+      e.classList.remove("invert"); // Ensure it resets
+    });
+
+    icon.src = "./svgs/darkIcon.svg";
+    icon.classList.remove("invert");
   }
-  isLight = !isLight
+
+  function dark() {
+    body.style.backgroundColor = "#1D1E20";
+
+    p.forEach((e) => {
+      e.style.color = "rgb(173, 173, 173)";
+    });
+
+    h1.forEach((elem) => {
+      elem.style.color = "white";
+    });
+
+    a.forEach((e) => {
+      e.classList.add("invert");
+    });
+
+    icon.src = "./svgs/lighticon.svg";
+    icon.classList.add("invert");
+
+    document.documentElement.style.setProperty("--scollbar-bg", "#1a1a1a");
+    document.documentElement.style.setProperty("--scrollbar-thumb", "white");
+  }
 });
-
-function light() {
-  body.style.backgroundColor = "#FAF9F6";
-
-  p.forEach((e) => {
-    e.style.color = "rgb(65, 65, 65)";
-  });
-
-  h1.style.color = "black";
-
-  a.forEach((e) => {
-    e.classList.remove("invert"); // Ensure it resets
-  });
-  
-  icon.src = "./svgs/darkIcon.svg";
-  icon.classList.remove("invert");
-}
-
-function dark() {
-  body.style.backgroundColor = "#1D1E20";
-
-  p.forEach(e=>{
-    e.style.color = "rgb(173, 173, 173)"
-  })
-
-  h1.style.color = "white";
-
-  a.forEach((e) => {
-    e.classList.add("invert");
-  });
-
-  icon.src= "./svgs/lighticon.svg";
-  icon.classList.add("invert")
-}
-
